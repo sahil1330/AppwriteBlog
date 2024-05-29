cc7b718c160371dee2e41f8f9022ee42cf8db8f3
sahil1330 <sahilmane025@gmail.com>
2024-05-30 01:53:02 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import AllPosts from "./AllPosts";
+import AddPost from "./AddPost";
+import EditPost from "./EditPost";
+import Post from "./Post";
+
+export { Home, Login, Signup, AllPosts, AddPost, EditPost, Post };
